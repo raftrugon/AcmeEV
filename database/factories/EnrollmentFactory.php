3fc46891970cce9cb3585b77dab706a7851eb533
César García <cgpcsjcmp@gmail.com>
2018-05-17 14:17:21 +0200

    ADD: Added subjectInstance seeding based on existing subjects. Subject seeder factory number of items reduced. UPDATE: Moved subject factory method from UserFactory to SubjectFactory.

--- database/factories/EnrollmentFactory.php
+++ database/factories/EnrollmentFactory.php
@@ -21,7 +21,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Enrollment::class, function (Faker $faker) {
     return [
         'subject_instance_id'=>$faker->randomElement(SubjectInstance::all()->pluck('id')->toArray()),
-        //'user_id'=>$faker->randomElement(User::all()->pluck('id')->toArray()),
         'user_id'=>$faker->randomElement(User::join('model_has_permissions','users.id','=','model_has_permissions.model_id')->where('model_has_permissions.permission_id', 2)->get()->pluck('id')->toArray())
     ];
 });
--- database/seeds/SubjectInstanceSeeder.php
+++ database/seeds/SubjectInstanceSeeder.php
@@ -1,12 +1,31 @@
 <?php
 
+use App\Subject;
 use App\SubjectInstance;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
+
 
 class SubjectInstanceSeeder extends Seeder{
 
     public function run(){
-        SubjectInstance::firstOrCreate(['id'=>1,'academic_year'=>2019,'subject_id'=>1]);
-        SubjectInstance::firstOrCreate(['id'=>2,'academic_year'=>2019,'subject_id'=>2]);
+        //SubjectInstance::firstOrCreate(['id'=>1,'academic_year'=>2019,'subject_id'=>1]);
+        //SubjectInstance::firstOrCreate(['id'=>2,'academic_year'=>2019,'subject_id'=>2]);
+
+        $faker = Factory::create();
+        $id = 1;
+        $subjects_id = Subject::all()->pluck('id')->toArray();
+
+        //for($i = 0; $i < 10; $i++){
+        foreach($subjects_id as $subject_id){
+            $academic_year = $faker->numberBetween(2008, 2013);                                    //Año de comienzo de la asignatura
+            $endFor = date('Y');                                                            //Año actual
+
+
+            for($j = $academic_year; $j < $endFor; $j++){                                          //Crea instancias desde el año de comienzo hasta el actual
+                SubjectInstance::firstOrCreate(['id'=>$id,'academic_year'=>$j,'subject_id'=>$subject_id]);
+                $id++;
+            }
+        }
     }
 }
