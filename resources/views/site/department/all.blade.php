75049b2fe0afedb0f28a4baff551a22a4a069245
César García <cgpcsjcmp@gmail.com>
2018-05-21 14:23:46 +0200

    Routes are better ordered and departments list is working fine.

--- resources/views/site/department/all.blade.php
+++ resources/views/site/department/all.blade.php
@@ -3,20 +3,23 @@
 @section('content')
 
     <div class="card-deck">
-        @foreach($departments as $department)
-            <div class="card">
-                <h5 class="card-header">
-                    <a href="{{URL::to('department/'.$department->getId().'/display')}}">{{$department->getName()}}</a>
-                </h5>
-                <div class="card-body">
-                    <p class="card-text">
-                        <strong>@lang('department.code'): </strong> {{$department->getCode()  }}</br>
-                        <strong>@lang('department.url'): </strong>
-                                {{$department->getWebsite()}}
-                    </p>
+        <div class="row">
+            @foreach($departments as $department)
+                <div class="col-md-6" style="padding: 20px;">
+                <div class="card">
+                    <h5 class="card-header">
+                        <a href="{{URL::to('department/'.$department->getId().'/display')}}">{{$department->getName()}}</a>
+                    </h5>
+                    <div class="card-body">
+                        <p class="card-text">
+                            <strong>@lang('department.code'): </strong> {{$department->getCode()  }}</br>
+                            <strong>@lang('department.url'): </strong> {{$department->getWebsite()}}
+                        </p>
+                    </div>
                 </div>
-            </div>
-        @endforeach
+                </div>
+            @endforeach
+        </div>
     </div>
 
 @endsection
