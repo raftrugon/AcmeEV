4ffc9f82a6d714ae7958eb6fa95c738bdf0f34f9
César García <cgpcsjcmp@gmail.com>
2018-05-23 20:48:38 +0200

    WIP: EnrollmentAndMinutesSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,23 +15,24 @@ class DatabaseSeeder extends Seeder
         $this->call(DegreeSeeder::class);
         $this->call(DepartmentSeeder::class);
         $this->call(UsersAndRolesSeeder::class);
+        $this->call(RoomSeeder::class);
 
-        $this->call(DegreeSeeder::class);
+        //$this->call(DegreeSeeder::class);
         $this->call(DepartmentSeeder::class);                   //Basado en degrees
         $this->call(LecturersFromDepartmentsSeeder::class);     //Basado en departments
 
         $this->call(SubjectSeeder::class);                      //Basado en degrees
         $this->call(SubjectInstanceSeeder::class);              //Basado en subjects
-        $this->call(AnnouncementSeeder::class);                 //Basado ene subject instances
         $this->call(GroupSeeder::class);                        //Basado en subject instances
-        $this->call(RoomSeeder::class);
         $this->call(ControlCheckSeeder::class);
 
         $this->call(StudentsSeeder::class);
         //TO DO  $this->call(EnrollmentAndMinuteSeeder::class);        //Basado en subject instances //Varias iteraciones de creación -> enrollments, minutes, enrollments, minutes...
+        $this->call(EnrollmentSeeder::class);                   //Será sustituido por EnrollmentAndMinuteSeeder
 
         $this->call(InscriptionSeeder::class);
-        //$this->call(EnrollmentSeeder::class);                   //Será sustituido por EnrollmentAndMinuteSeeder
+        $this->call(AnnouncementSeeder::class);                 //Basado ene subject instances
+
 
 
     }
--- database/seeds/EnrollmentSeeder.php
+++ database/seeds/EnrollmentSeeder.php
@@ -4,6 +4,7 @@ use App\Enrollment;
 use App\Subject;
 use App\SubjectInstance;
 use App\User;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class EnrollmentSeeder extends Seeder
@@ -35,17 +36,17 @@ class EnrollmentSeeder extends Seeder
             $actual_academic_year++;
 
 
-        $students = User::join('model_has_permissions','users.id','=','model_has_permissions.model_id')->where('model_has_permissions.permission_id', 2)->whereNotNull('degree_id')->get()->toArray();
+        $students = User::join('model_has_permissions','users.id','=','model_has_permissions.model_id')->where('model_has_permissions.permission_id', 2)->whereNotNull('users.degree_id')->select('users.*')->get();
 
         foreach ($students as $student){
 
             //Varibales
 
             $degree = $student->getDegree;
-            $subjects = $degree->getSubjects();
+            //$subjects = $degree->getSubjects();
 
 
-            $starting_academic_year = $faker->randomBetween($min_academic_year, $max_academic_year);
+            $starting_academic_year = $faker->numberBetween($min_academic_year, $max_academic_year);
 
             for($i = $starting_academic_year; $i <= $actual_academic_year; $i++){
 
@@ -60,18 +61,18 @@ class EnrollmentSeeder extends Seeder
                     ->where('minutes.qualification', '>=', 5)                                                           //Filtra a las asignaturas con enrollments con minutes aprobados
                     ->select('subjects.*')->get()->pluck('id')->toArray();                                              //Extrae los IDs de esas asignaturas
 
-                $not_passed_subjects = SubjectInstance::where('academic_year', $i)                                      //Busca las subject_instances con año académico actual
+                $not_passed_subjects_ids = SubjectInstance::where('academic_year', $i)                                      //Busca las subject_instances con año académico actual
                     ->join('subjects', 'subject_instances.subject_id', '=', 'subjects.id')                              //Une con la tabla subjects
                     ->where('subjects.degree_id', $degree->getId())                                                     //Filtra a los subject_instances de asignaturas de la carrera del estudiante
                     ->whereNotIn('subjects.id', $passed_subjects_ids)                                                   //Retiene las asignaturas que no ha pasado aun
-                    ->get()->pluck('id')->toArray();                                                                    //Extrae las asignaturas
+                    ->select('subject_instances.*')->get()->pluck('id')->toArray();                                                                    //Extrae los IDs de esas instancias de asignaturas
 
 
 
                 //Generación de enrollments
 
                 for($i = 0; $i <= $number_of_enrollments_this_year; $i++){
-                    $subject_instance_id = $faker->randomElement($not_passed_subjects);
+                    $subject_instance_id = $faker->randomElement($not_passed_subjects_ids);
 
                     //Generación de enrollments
 
@@ -80,8 +81,9 @@ class EnrollmentSeeder extends Seeder
                         'user_id'=>$student->getId()
                     ]);
                     //Asignación a grupos
-                    $group = $faker->randomElement(SubjectInstance::where('id',$subject_instance_id)->get->getGroups());
-                    $student->getGroups()->attach($group->getId());
+                    $group_id = $faker->randomElement(SubjectInstance::where('id',$subject_instance_id)->first()->getGroups->pluck('id')->toArray());
+                    info($group_id);
+                    $student->getGroups()->attach($group_id);
 
                     //Generación de minutes
                     if($i != $actual_academic_year){
