7fc5c6f28af3fd4f9387d9cfd3c4dd730c0af572
César García <cgpcsjcmp@gmail.com>
2018-06-03 18:41:17 +0200

    FIX: Groups are now assigned from state 4 instead of 3.

--- app/Policies/SystemConfigPolicy.php
+++ app/Policies/SystemConfigPolicy.php
@@ -57,7 +57,7 @@ class SystemConfigPolicy
         $return = true;
         $actual_state = $this->systemConfigRepo->getActualState();
 
-        if($actual_state < 3 || $actual_state == 8)
+        if($actual_state < 4 || $actual_state == 8)
             $return = false;
 
         return $return;
