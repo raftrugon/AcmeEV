fd198cbf9f52c11b926324a1fd95cfcc6dd308a2
César García <cgpcsjcmp@gmail.com>
2018-06-07 14:03:21 +0200

    WIP: Change pickers due to validation display bugs. FIX: Validation working now properly

--- resources/views/site/subject/create-edit.blade.php
+++ resources/views/site/subject/create-edit.blade.php
@@ -16,7 +16,7 @@
                             <input type="hidden" name="id" value="{{$subject->getId()}}"/>
                             @include('global.input',['type'=>'text','id'=>'name','name'=>'name','label'=>__('attributes.name'),'value'=>$subject->getName()])
                             @include('global.input',['type'=>'number','id'=>'school_year','name'=>'school_year','label'=>__('attributes.school_year'),'value'=>$subject->getSchoolYear()])
-                            <div class="form-group">
+                            <div class="form-group"  style="margin-left:15px;margin-right: 15px;">
                                 <label for="select">@lang('attributes.semester')</label>
                                 <select class="form-control" id="selectSemester" name="semester">
                                     <option value="0" @if($subject->getSemester()===0) selected @endif>@lang('subject.first')</option>
@@ -24,7 +24,7 @@
                                     <option value="2" @if(is_null($subject->getSemester())) selected @endif>@lang('subject.annual')</option>
                                 </select>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group"  style="margin-left:15px;margin-right: 15px;">
                                 <label for="select">@lang('attributes.type')</label>
                                 <select class="form-control" id="selectType" name="type">
                                     <option value="0" @if($subject->getSubjectType()==='OBLIGATORY') selected @endif>@lang('subject.obligatory')</option>
@@ -38,14 +38,16 @@
                             @else
                                 @include('global.input',['type'=>'checkbox','value'=>1,'id'=>'active','name'=>'active','label'=>__('subject.active'),'texto'=>''])
                             @endif
+                            <div  style="margin-left:15px;margin-right: 15px;">
                             @include('global.select',['id'=>'department','objects'=>$departments,'value'=>'id','display'=>'name','label'=>__('subject.department'),'selectedOption'=>$subject->getDepartment->getId()])
                             @include('global.select',['id'=>'coordinator','objects'=>$subject->getDepartment->getPDIs,'value'=>'id','display'=>'full_name','label'=>__('subject.coordinator'),'selectedOption'=>$subject->getCoordinator->getId()])
+                            </div>
                         @else
                             <input type="hidden" name="degree" value="{{$degree->getId()}}"/>
                             <input type="hidden" name="id" value="0"/>
                             @include('global.input',['type'=>'text','id'=>'name','name'=>'name','label'=>__('attributes.name')])
                             @include('global.input',['type'=>'number','id'=>'school_year','name'=>'school_year','label'=>__('attributes.school_year')])
-                            <div class="form-group">
+                            <div class="form-group"  style="margin-left:15px;margin-right: 15px;">
                                 <label for="select">@lang('attributes.semester')</label>
                                 <select class="form-control" id="selectSemester" name="semester">
                                     <option value="0">@lang('subject.first')</option>
@@ -53,7 +55,7 @@
                                     <option value="2">@lang('subject.annual')</option>
                                 </select>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group" style="margin-left:15px;margin-right: 15px;">
                                 <label for="select">@lang('attributes.type')</label>
                                 <select class="form-control" id="selectType" name="type">
                                     <option value="0">@lang('subject.obligatory')</option>
@@ -62,16 +64,20 @@
                                     <option value="3">@lang('subject.dt')</option>
                                 </select>
                             </div>
+
                             @include('global.input',['type'=>'checkbox','value'=>1,'id'=>'active','name'=>'active','checked'=>'checked','label'=>__('subject.active'),'texto'=>''])
+                            <div  style="margin-left:15px;margin-right: 15px;">
                             @include('global.select',['id'=>'department','objects'=>$departments,'value'=>'id','display'=>'name','label'=>__('subject.department'),'default'=>__('global.choose_one')])
-                            <div class="form-group selectpicker-form-group">
-                               <label class="control-label">@lang('subject.coordinator')</label>
-                                <select class="selectpicker form-control" id="coordinator" name="coordinator">
-                                  <option value="">@lang('subject.select_department')</option>
-                                </select>
+
+                                <div class="form-group selectpicker-form-group">
+                                   <label class="control-label">@lang('subject.coordinator')</label>
+                                    <select class="selectpicker form-control" id="coordinator" name="coordinator">
+                                      <option value="">@lang('subject.select_department')</option>
+                                    </select>
+                                </div>
                             </div>
                         @endif
-                            <button class="btn btn-success">@lang('global.submit')</button>
+                            <button class="btn btn-success"  style="margin-left:15px;margin-right: 15px;">@lang('global.submit')</button>
                     </div>
                 </div>
             </div>
@@ -89,12 +95,25 @@
             $('#subject_form').validate({
                 rules: {
                     name: 'required',
-                    school_year: 'required'
+                    school_year:{
+                        required: true,
+                        min: 0,
+                        max: 6,
+                    },
+                    department : 'required',
+                    semester : 'required',
+                    active : 'required',
+                    coordinator : 'required',
+                    degree : 'required',
                 },
 
                 messages: {
                     name: "{{__('validation.required',['attribute'=>'attributes.name'])}}",
-                    school_year: "{{__('validation.required',['attribute'=>__('school_year')])}}",
+                    school_year:{
+                        required: "{{__('validation.required',['attribute'=>__('attributes.school_year')])}}",
+                        min: "{{__('validation.min.numeric',['attribute'=>__('attributes.school_year'),'min'=>0])}}",
+                        max: "{{__('validation.max.numeric',['attribute'=>__('attributes.school_year'),'max'=>6])}}",
+                    },
                 }
             });
 
